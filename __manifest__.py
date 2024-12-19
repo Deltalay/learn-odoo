00aff7bb12ff7234129f4f92e10dfe45a4e70dcc
Deltalay <paylayhok168@gmail.com>
2024-12-19 11:20:52 +0700

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+	'name': "Real Estate Ads",
+	'version': '0.0.1',
+	'author': 'Delta',
+	'category': 'Human Resources',
+	'description': """
+	Real estate stuff
+	""",
+	'installable': True,
+	'application': True,
+	'data': [
+		'security/ir.model.access.csv'
+	]
+}
---
+++ models/property.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+class TestModel(models.Model):
+	_name = 'estate.property'
+	_description = "Real estate property"
+	name = fields.Char(string="Name")
+	description = fields.Text(string="Description")
+	postcode = fields.Char(string="Postcode")
+	data_availability = fields.Date(string="Available From")
+	expected_price = fields.Float(string="Expected Price")
+	selling_price = fields.Float(string="Selling Price")
+	bedrooms = fields.Integer(string="Bedrooms")
+	living_area = fields.Integer(string="Living Area")
+	facades = fields.Integer(string="Facades")
+	garage = fields.Boolean(string="Garage")
+	garden = fields.Boolean(string="Garden")
+	garden_area = fields.Integer(string="Garden Area")
+	garden_orientation = fields.Selection(string="Garden Orientation", selection=[('north', 'North'), ('west', 'West'), ('south', "South"), ('east', "East")], help="This is your garden orientation. You can use compass to check it.")
