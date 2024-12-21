b1f1ae8869fb3d95384fc952771bae1d3d9da472
Deltalay <paylayhok168@gmail.com>
2024-12-21 09:50:41 +0700

    property offer

---
+++ models/property_offer.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+class PropertyOffer(models.Model):
+	_name = "estate.property.offer"
+	_description = "Property Offer"
+	partner_id = fields.Many2one('res.partner', string="Customer")
+	property_id = fields.Many2one('estate.property', string="Property")
+	price = fields.Float(string="Price")
+	status = fields.Selection(string="Status", 
+		selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
