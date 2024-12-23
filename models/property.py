b6bec4bf1ded26b82331931d6f1d5b59208e649f
Deltalay <paylayhok168@gmail.com>
2024-12-23 09:33:27 +0700

    Add stuff

--- models/property.py
+++ models/property.py
@@ -22,11 +22,10 @@ class Property(models.Model):
 	offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
 	sales_id = fields.Many2one('res.users', string="Salesman")
 	buyer_id = fields.Many2one("res.partner", string="Buyer")
-	@api.depends('living_area', 'garden_area')
-	def _compute_total_area(self):
-		for rec in self:
-			rec.total_area = rec .living_area + rec.garden_area
-	total_area = fields.Integer(string="Total Area", compute=_compute_total_area)
+	@api.onchange('living_area', 'garden_area')
+	def _onchange_total_area(self):
+		self.total_area = self.living_area + self.garden_area
+	total_area = fields.Integer(string="Total Area")
 
 class PropertyType(models.Model):
 	_name= 'estate.property.type'
