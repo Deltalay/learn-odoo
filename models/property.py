aec64e3b5e5ad2f75004f2ac7e95ee5555339dab
Deltalay <paylayhok168@gmail.com>
2024-12-25 09:16:19 +0700

    Add phone

--- models/property.py
+++ models/property.py
@@ -22,6 +22,7 @@ class Property(models.Model):
 	offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
 	sales_id = fields.Many2one('res.users', string="Salesman")
 	buyer_id = fields.Many2one("res.partner", string="Buyer")
+	phone = fields.Char(string="Phone", related='buyer_id.phone')
 	@api.onchange('living_area', 'garden_area')
 	def _onchange_total_area(self):
 		self.total_area = self.living_area + self.garden_area
